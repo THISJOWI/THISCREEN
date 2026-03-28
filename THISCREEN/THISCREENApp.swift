6de543e59db31e3c1917d629949f235c8b405978
THISJowi <thsjowi@gmail.com>
2026-03-28 14:07:30 +0100

    Initial Commit

---
+++ THISCREEN/THISCREENApp.swift
@@ -0,0 +1,17 @@
+//
+//  THISCREENApp.swift
+//  THISCREEN
+//
+//  Created by joel mendez cruz on 28/3/26.
+//
+
+import SwiftUI
+
+@main
+struct THISCREENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
